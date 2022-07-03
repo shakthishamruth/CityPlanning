38afc46cacd0d420db644ec18bb3e217299a2363
Shakthi <shakthishamruth@gmail.com>
2022-07-04 02:23:15 +0530

    Updated test_r.py

--- test_r.py
+++ test_r.py
@@ -29,7 +29,16 @@ for i in range(int((500 / x) ** 2)):  # A for loop for row entries
         a.append(int(0))
     matrix.append(a)
 
-running = True
+# Algorithm to generate city
+residential = 35
+while not residential == 0:
+    for i in range(1, 10):  # A for loop for row entries
+        for j in range(1, 10):  # A for loop for column entries
+            pick_num_i = 0
+            pick_num_j = 0
+            if not matrix[i - pick_num_i][j - pick_num_j] == 1:
+                matrix[i - pick_num_i][j - pick_num_j] = 1
+                residential -= 1
 
 
 # funcs
@@ -42,13 +51,13 @@ def drawGrid(n):
 
 
 def fillGrid(n):
-    global matrix
+    global matrix, blockSize
     a = 0
     b = 0
     blockSize = n - 2  # Set the size of the grid block
     for i in range(0, WINDOW_WIDTH, blockSize + 2):
         for j in range(0, WINDOW_HEIGHT, blockSize + 2):
-            if (matrix[a][b] <= 35):  # green
+            if (matrix[a][b] == 1):  # green
                 rect = pygame.Rect(i, j, blockSize, blockSize)
                 pygame.draw.rect(SCREEN, (0, 255, 0), rect)
             elif matrix[a][b] >= 35 and matrix[a][b] <= 56:  # blue
@@ -57,9 +66,14 @@ def fillGrid(n):
             elif matrix[a][b] >= 56 and matrix[a][b] <= 70:  # yellow
                 rect = pygame.Rect(i, j, blockSize, blockSize)
                 pygame.draw.rect(SCREEN, (255, 241, 31), rect)
+            elif matrix[a][b] == 0:
+                rect = pygame.Rect(i, j, blockSize, blockSize)
+                pygame.draw.rect(SCREEN, (255, 255, 255), rect)
             a = a + 1
             b = b + 1
 
+
+running = True
 
 # main loop
 while running:
